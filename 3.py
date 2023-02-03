b53f906e8f38203ea07fd25c1b10dd098b91925f
nore-mac <camlake122@gmail.com>
2023-02-03 02:02:12 +0000

    first commit

---
+++ 3.py
@@ -0,0 +1,32 @@
+"""
+Project Euler: Problem 3
+
+"The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?"
+
+First we must construct a list of prime numbers up to the square root of 600851475143
+"""
+
+import math
+
+num = 600851475143
+root = math.ceil(math.sqrt(num))
+primes = [2]
+
+for i in range(3, root, 2):
+    for j in range(len(primes)):
+        if i % primes[j] != 0:
+            counter = 1
+        else:
+            counter = 0
+            break
+    if counter == 1:
+        primes.append(i)
+
+for i in reversed(primes):
+    if num % i == 0:
+        print(i)
+        break
+    else:
+        continue
